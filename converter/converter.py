3f80478231252f163311b538940e6f559b5adb9a
Rachael DeMaida <rg.NCC1701D@gmail.com>
2017-03-28 16:53:09 -0400

    Update converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -15,9 +15,10 @@ class Converter:
 
     @commands.command()
     async def convertf(self, num:Decimal):
-
+        msg = str(num*1.8+32)
+        
         #Your code will go here
-        await self.bot.say(to_fahrenheit)
+        await self.bot.say(msg)
 
 def setup(bot):
     bot.add_cog(Converter(bot))
