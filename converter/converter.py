54f43f11355d9803f1bf109b80c0f4306dc5d266
Rachael Grabowski <rg.NCC1701D@gmail.com>
2017-03-28 16:33:02 -0400

    Update converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -3,16 +3,21 @@ from discord.ext import commands
 
 class Converter:
     """My custom cog that does stuff!"""
-
+    
+    def _to_fahrenheit(self, num):
+        return str((num*1.8+32))
+    
+    def _to_celcius(self, num):
+        return str((num - 32)*.5556)
+    
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def convert(self):
-        """This does stuff!"""
+    async def convertf(self, num:dec):
 
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+        await self.bot.say(_to_fahrenheit)
 
 def setup(bot):
     bot.add_cog(Converter(bot))
