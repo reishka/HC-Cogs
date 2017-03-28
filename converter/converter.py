af4248057d9fe77244e1009580b2480a0d83748f
Rachael DeMaida <rg.NCC1701D@gmail.com>
2017-03-28 17:00:05 -0400

    Update converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -1,23 +1,29 @@
 import discord
 from discord.ext import commands
+from __main__ import send_cmd_help
 from decimal import Decimal
 
+
 class Converter:
-    
-    def to_fahrenheit(self, num:Decimal):
-        return str((num*1.8+32))
-    
-    def to_celcius(self, num:Decimal):
-        return str((num - 32)*.5556)
-    
+
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def convertf(self, num:Decimal):
-        msg = str(num*1.8+32)
         
-        #Your code will go here
+    @commands.group(pass_context=True, no_pm=True)
+    async def convert(self, ctx):
+        """Converter settings group command"""
+        
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @freedom.command(name="freedom", pass_context=True)
+    async def (self, ctx, amount:Decimal)
+        msg = str(amount*1.8+32)
+        await self.bot.say(msg)
+        
+    @commie.command(name="commie", pass_context=True)
+    async def (self, ctx, amount:Decimal)
+        msg = str((amount-32)*.5556)
         await self.bot.say(msg)
 
 def setup(bot):
