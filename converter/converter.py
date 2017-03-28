601f76dc64dce31278adad9a4277650fbef5d8a4
Rachael DeMaida <rg.NCC1701D@gmail.com>
2017-03-28 17:45:31 -0400

    Update converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -9,20 +9,20 @@ class Converter:
     def __init__(self, bot):
         self.bot = bot
         
-   @commands.group(pass_context=True, no_pm=True, manage_server=False)
-   async def convert(self, ctx):
+    @commands.group()
+    async def convert(self, ctx):
         """Converter settings group command"""
         
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
-   
-   @convert.command(name="freedom", pass_context=True, manage_server=False)
-   async def freedom(self, *message):
+    
+    @convert.command(name="freedom", pass_context=True, manage_server=False)
+    async def freedom(self, *message):
         msg = str(float(message[0])*1.8+32)
         await self.bot.say(msg)
         
-   @convert.command(name="commie", pass_context=True, manage_server=False)
-   async def commie(self, ctx, amount:str):
+    @convert.command(name="commie", pass_context=True, manage_server=False)
+    async def commie(self, ctx, amount:str):
         msg = str((float(amount)-32)*.5556)
         await self.bot.say(msg)
 
