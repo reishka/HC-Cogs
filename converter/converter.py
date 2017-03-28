d9234243182bb675b36ea1e1f45259e88bb6790b
Rachael DeMaida <rg.NCC1701D@gmail.com>
2017-03-28 17:43:14 -0400

    Update converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -17,8 +17,8 @@ class Converter:
             await send_cmd_help(ctx)
    
    @convert.command(name="freedom", pass_context=True, manage_server=False)
-   async def freedom(self, ctx, amount:str):
-        msg = str(float(amount)*1.8+32)
+   async def freedom(self, *message):
+        msg = str(float(message[0])*1.8+32)
         await self.bot.say(msg)
         
    @convert.command(name="commie", pass_context=True, manage_server=False)
