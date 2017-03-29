274d051732571d4d25665b6a2ace5846c23b4fbc
Sébastien Ouellet <monster3d28@hotmail.com>
2017-03-29 00:33:46 -0400

    Start cogs and try out parsing messages

--- trickery/trickery.py
+++ trickery/trickery.py
@@ -19,28 +19,9 @@ class GeneralTrickery:
                       aliases=["trickeries"])
     async def _trickery(self, ctx: commands.Context):
         """Doing whatever."""
-
-        base_text1 = "# - O \n"
-        base_text2 = "# - X"
-        await self.bot.say("Let's get this race on!")
-        msg = await self.bot.say(base_text1+base_text2)  
-        i = 0
-        length_1 = 0
-        length_2 = 0
-        while i < 40: 
-            await asyncio.sleep(1)
-            length_1 += random.randint(1,5)
-            length_2 += random.randint(1,5)
-            text1 = base_text1.replace('-','-'*length_1)
-            text2 = base_text2.replace('-','-'*length_2)
-            await self.bot.edit_message(msg, text1+text2)
-            if len(text1) > 40:
-                await self.bot.say("Player 1 is victorious!")
-                return
-            if len(text2) > 40:
-                await self.bot.say("Player 2 is victorious!")
-                return
-            i += 1
+        await self.bot.say("Gotta repeat messages here")
+        async for message in self.bot.logs_from("the_lounge",10):
+            await self.bot.say(message.content)
 
 def setup(bot: commands.Bot):
     bot.add_cog(GeneralTrickery(bot))
