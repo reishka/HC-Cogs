5713dd5798361b0dfdfe323aa82fa2f76f81db89
PeenBot <peen@nadekobot.localdomain>
2017-03-29 16:45:09 +0000

    SPACES

--- converter/Converter.py
+++ converter/Converter.py
@@ -1,26 +1,26 @@
 import discord
+import pudb
 from discord.ext import commands
 from __main__ import send_cmd_help
-from decimal import Decimal
-
 
 class Converter:
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(pass_context=True)
-    async def convert(ctx):
-        if ctx.invoked_subcommand is None:
-            '''await send_cmd_help(ctx)'''
-            await bot.say('Invalid convert parameter')
-    
+    @commands.group(name="convert", pass_context=True)
+    async def convert(self, ctx):
+
+     """Invoke help if there is no command"""
+     if ctx.invoked_subcommand is None:
+         await send_cmd_help(ctx)
+
     @convert.command(name='freedom', pass_context=True)
-    async def _freedom(ctx):
+    async def freedom(ctx):
         await self.bot.say(ctx + " in Commie units is " + str(float(ctx)*1.8+32) + " freedom units.")
-        
+
     @convert.command(name="commie", pass_context=True)
-    async def _commie(ctx):
+    async def commie(ctx):
         await self.bot.say(ctx +" in Freedom Units is " + str(float(ctx)-32*.5556) + " commies.")
 
 def setup(bot):
