463852586abe7ad5efcf0c84adb4b971e8dd1a09
Gihzah <breault.vincent@gmail.com>
2017-03-29 03:51:43 +0000

    Tutorial commands

--- mycog2/mycog2.py
+++ mycog2/mycog2.py
@@ -1,5 +1,12 @@
 import discord
 from discord.ext import commands
+try:
+    from bs4 import BeautifulSoup
+    soupAvailable = True
+except ModuleNotFound:
+    soupAvailable = False
+import aiohttp
+
 
 class Mycog2:
     """My custom cog that does stuff!"""
@@ -9,10 +16,37 @@ class Mycog2:
 
     @commands.command()
     async def gihcom(self):
-        """This does stuff!"""
+        """Command takes no argument, says line."""
 
         #Your code will go here
         await self.bot.say("I can do stuff!")
+        
+    @commands.command()
+    async def punch(self, user : discord.Member):
+        """Command takes argument, adds it to string to say."""
+    
+        #Your code will go here
+        await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+
+    @commands.command()
+    async def getItem(self):
+        """Command takes argument, adds it to string to say."""
+        
+        url = 'https://api.xivdb.com/item/17433'
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), 'html.parser')
+        import pdb; pdb.set_trace()
+        try:
+            online = 
+        #Your code will go here
+        await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+    
+
+
+
 
 def setup(bot):
-    bot.add_cog(Mycog2(bot))
+    if soupAvailable:
+        bot.add_cog(Mycog2(bot))
+    else:
+        raise RuntimeError("You need to run `pip3 install beautifulsoup4`")
