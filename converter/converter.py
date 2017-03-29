838a4c79f5942553193cb5d4ad576dd2811a0027
Rachael DeMaida <rg.NCC1701D@gmail.com>
2017-03-29 13:42:26 -0400

    Update converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -16,12 +16,12 @@ class Converter:
             await send_cmd_help(ctx)
 
     @convert.command(name='freedom', pass_context=True)
-    async def freedom(self, ctx, units:str):
-        await self.bot.say(units + " in Commie units is " + str(float(units)*1.8+32) + " freedom units.")
+    async def freedom(self, ctx):
+        await self.bot.say(str(ctx) + " in Commie units is " + str(float(ctx)*1.8+32) + " freedom units.")
 
     @convert.command(name="commie", pass_context=True)
     async def commie(self, ctx):
-        await self.bot.say(ctx +" in Freedom Units is " + str(float(ctx)-32*.5556) + " commies.")
+        await self.bot.say(str(ctx) +" in Freedom Units is " + str(float(ctx)-32*.5556) + " commies.")
 
 def setup(bot):
     bot.add_cog(Converter(bot))
