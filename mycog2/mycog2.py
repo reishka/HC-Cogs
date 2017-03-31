75f62ba38abf9fccb1f7d354a7f191756f409d3d
Gihzah <breault.vincent@gmail.com>
2017-03-31 01:43:51 +0000

    Testing around with resquest.

--- mycog2/mycog2.py
+++ mycog2/mycog2.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.errors import HTTPException
 try:
     from bs4 import BeautifulSoup
     soupAvailable = True
@@ -11,8 +12,9 @@ import aiohttp
 class Mycog2:
     """My custom cog that does stuff!"""
 
-    def __init__(self, bot):
+    def __init__(self, bot, BASE_XIV_API = 'https://api.xivdb.com/'):
         self.bot = bot
+        self.BASE_XIV_API = BASE_XIV_API
 
     @commands.command()
     async def gihcom(self):
@@ -32,15 +34,27 @@ class Mycog2:
     async def getItem(self):
         """Command takes argument, adds it to string to say."""
         
-        url = 'https://api.xivdb.com/item/17433'
+        url = self.BASE_XIV_API+'item/17433'
         async with aiohttp.get(url) as response:
-            soupObject = BeautifulSoup(await response.text(), 'html.parser')
-        import pdb; pdb.set_trace()
+            # soupObject = BeautifulSoup(await response.text(), 'html.parser')
+            # textObject = await response.text()
+            if response.status == 200:
+                jsonObject = await response.json()
+            
+        # import pdb; pdb.set_trace()
+        # print(soupObject)
+        # print(textObject)
+        # print(textObject.__class__)
         try:
-            online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
-            await self.bot.say(online + ' players are playing this game at the moment')
-        except:
-            await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
+            await self.bot.say(jsonObject)
+        except HTTPException:
+            raise 'textObject too long. Character limit is 2000'
+            
+        # try:
+        #     online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
+        #     await self.bot.say(online + ' players are playing this game at the moment')
+        # except:
+        #     await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
 
 
 def setup(bot):
