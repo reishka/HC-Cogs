8b4a61be689e6d6432ff2062068eabf0bee0fcd3
Perth <rg.NCC1701D@gmail.com>
2017-04-14 14:24:39 -0400

    Key error

--- dice/dice.py
+++ dice/dice.py
@@ -9,16 +9,16 @@ class Dice:
 		self.bot = bot
 		self.roll_arr = []
 		self.discord_arr =[]
-		self.discord_num={0:':zero:',
-				   1:':one:',
-				   2:':two:',
-				   3:':three:',
-				   4:':four:',
-				   5:':five:',
-				   6:':six:',
-				   7:':seven:',
-				   8:':eight:',
-				   9:':nine:'}
+		self.discord_num={'0':':zero:',
+				   '1':':one:',
+				   '2':':two:',
+				   '3':':three:',
+				   '4':':four:',
+				   '5':':five:',
+				   '6':':six:',
+				   '7':':seven:',
+				   '8':':eight:',
+				   '9':':nine:'}
 
 	def roll_dice(self, dice, sides, result):
 
