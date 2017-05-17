161fdc30f68435999998228874dfeb37d986830d
Perth <nadekobot.localdomain>
2017-05-17 23:38:04 +0000

    Consolidated last updated file and merged it with new file: currencies. Currencies now has formatting for "native" display and currency markers.

--- converter/converter.py
+++ converter/converter.py
@@ -9,7 +9,6 @@ from .utils.dataIO import dataIO
 from discord.ext import commands
 from __main__ import send_cmd_help
 
-UPDATES_PATH = 'data/converter/updates.json'
 CURRENCIES_PATH = 'data/converter/currencies.json'
 RATES_PATH = 'data/converter/rates_'
 RATES_SUFF = '.json'
@@ -19,10 +18,13 @@ class Converter:
 	def __init__(self, bot):
 		self.bot = bot
 		self.input_message = "That's not a number!"     # Error message for garbage input
-		self.last_update = dataIO.load_json(UPDATES_PATH)
 		self.currencies = dataIO.load_json(CURRENCIES_PATH)
-
-		self.valid_currencies = ['AUD','BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'GBP', 'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR', 'EUR']
+		self.valid_currencies = ['AUD','BGN', 'BRL', 'CAD', 'CHF', 'CNY',
+					 'CZK', 'DKK', 'GBP', 'HKD', 'HRK', 'HUF',
+					 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN',
+					 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON',
+					 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'USD',
+					 'ZAR', 'EUR']
 
 	def is_number(self, s):
 		try:
@@ -41,41 +43,42 @@ class Converter:
 		rates_file_path = RATES_PATH + currency_from + RATES_SUFF
 		currency_f = str.upper(currency_from)
 
-		if self.last_update[currency_f] is not None:
-			update_delta = datetime.now() - datetime.strptime(self.last_update[currency_f], '%Y-%m-%d %H:%M:%S.%f')
+		if self.currencies[currency_f]["last_updt"] is not None:
+			update_delta = datetime.now() - datetime.strptime(self.currencies[currency_f]["last_updt"], '%Y-%m-%d %H:%M:%S.%f')
 
 			if update_delta.seconds > 86400:
 				rates = self.get_rates(currency_f)
 				dataIO.save_json(rates_file_path, rates)
-				self.last_update[currency_f] = str(datetime.now())
-				dataIO.save_json(UPDATES_PATH, self.last_update)
+				self.currencies[currency_f]["last_updt"] = str(datetime.now())
+				dataIO.save_json(CURRENCIES_PATH, self.currencies)
 			else:
 				rates = dataIO.load_json(rates_file_path)
 
 		else:
 			rates = self.get_rates(currency_f)
 			dataIO.save_json(rates_file_path, rates)
-			self.last_update[currency_f] = str(datetime.now())
-			dataIO.save_json(UPDATES_PATH, self.last_update)
+			self.currencies[currency_f]["last_updt"] = str(datetime.now())
+			dataIO.save_json(CURRENCIES_PATH, self.currencies)
 
 		return rates['rates'][str.upper(currency_to)]
 
-	def formatter(self, amount:Decimal, currency_to:str)
+	def formatter(self, amount:Decimal, currency_to:str):
 
 		symbol = self.currencies[str.upper(currency_to)]["symbol"]
 		spacer = self.currencies[str.upper(currency_to)]["sep_1"]
-		decpt = self.currencies[str.upper(currency_to)]["sep_2"]
+		dec_pt = self.currencies[str.upper(currency_to)]["sep_2"]
 		minor = self.currencies[str.upper(currency_to)]["minor"]
-		quant = Decimal(10) ** -minor
-		sign, digits, exp = currency_to.quantize(quant).as_tuple()
+
+		quant = Decimal(10) ** -int(minor)
+		sign, digits, exp = amount.quantize(quant).as_tuple()
 		result = []
 
-		digits = map(str, digits)
+		digits = list(digits)
 		build, next = result.append, digits.pop
 
-		for i in range(currency_to):
+		for i in range(int(minor)):
 			build(next() if minor is not '0'  else '0')
-		build(decpt)
+		build(dec_pt)
 		if not digits:
 			build('0')
 		i = 0
@@ -86,6 +89,7 @@ class Converter:
 				i = 0
 				build(spacer)
 		build(symbol)
+		result = [str(item) for item in result]
 		return ''.join(reversed(result))
 
 	@commands.group(name="convert", pass_context=True)
@@ -222,9 +226,9 @@ class Converter:
 		if (str.upper(currency_from) in self.valid_currencies) and (str.upper(currency_to) in self.valid_currencies):
 
 			rate = self.get_exchange_rate(amount, currency_from, currency_to)
-			Decimal(conversion = amount * rate)
+			conversion = Decimal(amount * Decimal(rate))
 
-			await self.bot.say(str(formatter(conversion)))
+			await self.bot.say(str(self.formatter(conversion, currency_to)))
 		else:
 			await self.bot.say("Those might not be currencies.")
 
@@ -240,88 +244,50 @@ def check_folders():
 		os.makedirs("data/converter")
 
 def check_files():
-	if not dataIO.is_valid_json(UPDATES_PATH):
-		print("Creating default updates file...")
-		default_updates = {"AUD":None,
-				"BGN":None,
-				"BRL":None,
-				"CAD":None,
-				"CHF":None,
-				"CNY":None,
-				"CZK":None,
-				"DKK":None,
-				"GBP":None,
-				"HKD":None,
-				"HRK":None,
-				"HUF":None,
-				"IDR":None,
-				"ILS":None,
-				"INR":None,
-				"JPY":None,
-				"KRW":None,
-				"MXN":None,
-				"MYR":None,
-				"NOK":None,
-				"NZD":None,
-				"PHP":None,
-				"PLN":None,
-				"RON":None,
-				"RUB":None,
-				"SEK":None,
-				"SGD":None,
-				"THB":None,
-				"TRY":None,
-				"ZAR":None,
-				"USD":None,
-				"EUR":None}
-		dataIO.save_json(UPDATES_PATH, default_updates)	
-	
+
 	if not dataIO.is_valid_json(CURRENCIES_PATH):
 		print("Creating default currencies file...")
 		space = " "
 		dot = '.'
 		quote = "'"
 		comma = ','
-		default_countries = { "countries":[
-		{"AUD": 'symbol':'$', 	'sep_1':space, 	'sep_2':comma, 	'minor':'2'},
-		{"BGN": 'symbol':'лв', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},  //Not sure on this one
-		{"BRL": 'symbol':'R$', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2'},
-		{"CAD": 'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"CHF": 'symbol':'CHF', 'sep_1':quote, 	'sep_2':dot, 	'minor':'2'},
-		{"CNY": 'symbol':'¥', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"CZK": 'symbol':'Kč', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2'},
-		{"DKK": 'symbol':'kr', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2'},
-		{"EUR": 'symbol':'€', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"GBP": 'symbol':'£', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"HKD": 'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"HRK": 'symbol':'kn', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2'},
-		{"HUF": 'symbol':'Ft', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'0'},
-		{"IDR": 'symbol':'Rp', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2'},
-		{"ILS": 'symbol':'₪', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"INR": 'symbol':'₹', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"JPY": 'symbol':'¥', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'0'},
-		{"KRW": 'symbol':'₩', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'0'},
-		{"MXN": 'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"MYR": 'symbol':'RM', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"NOK": 'symbol':'kr', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2'},
-		{"NZD": 'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"PHP": 'symbol':'₱', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"PLN": 'symbol':'zł', 	'sep_1':space, 	'sep_2':comma, 	'minor':'2'},
-		{"RON": 'symbol':'lei', 'sep_1':dot, 	'sep_2':comma, 	'minor':'2'},
-		{"RUB": 'symbol':'₽', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2'},
-		{"SEK": 'symbol':'kr', 	'sep_1':space, 	'sep_2':comma, 	'minor':'2'},
-		{"SGD": 'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"THB": 'symbol':'฿', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"TRY": 'symbol':'‎₺', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"USD": 'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2'},
-		{"ZAR": 'symbol':'R', 	'sep_1':space, 	'sep_2':dot, 	'minor':'2'}]}
+		default_countries = {
+		"AUD": {'symbol':'$', 	'sep_1':space, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"BGN": {'symbol':'лв', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"BRL": {'symbol':'R$', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"CAD": {'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"CHF": {'symbol':'CHF', 'sep_1':quote, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"CNY": {'symbol':'¥', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"CZK": {'symbol':'Kč', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"DKK": {'symbol':'kr', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"EUR": {'symbol':'€', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"GBP": {'symbol':'£', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"HKD": {'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"HRK": {'symbol':'kn', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"HUF": {'symbol':'Ft', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'0', 'last_updt':None},
+		"IDR": {'symbol':'Rp', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"ILS": {'symbol':'₪', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"INR": {'symbol':'₹', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"JPY": {'symbol':'¥', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'0', 'last_updt':None},
+		"KRW": {'symbol':'₩', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'0', 'last_updt':None},
+		"MXN": {'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"MYR": {'symbol':'RM', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"NOK": {'symbol':'kr', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"NZD": {'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"PHP": {'symbol':'₱', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"PLN": {'symbol':'zł', 	'sep_1':space, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"RON": {'symbol':'lei',	'sep_1':dot, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"RUB": {'symbol':'₽', 	'sep_1':dot, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"SEK": {'symbol':'kr', 	'sep_1':space, 	'sep_2':comma, 	'minor':'2', 'last_updt':None},
+		"SGD": {'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"THB": {'symbol':'฿', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"TRY": {'symbol':'‎₺', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"USD": {'symbol':'$', 	'sep_1':comma, 	'sep_2':dot, 	'minor':'2', 'last_updt':None},
+		"ZAR": {'symbol':'R', 	'sep_1':space, 	'sep_2':dot, 	'minor':'2', 'last_updt':None}}
 
-		data_IO.save_json(CURRENCIES_PATH, default_countries)
+		dataIO.save_json(CURRENCIES_PATH, default_countries)
 
 def setup(bot):
-	if not babelAvailable:
-		raise RuntimeError("You need to run 'pip3 install Babel'")
-	else:
-		check_folders()
-		check_files()
-		bot.add_cog(Converter(bot))
+	check_folders()
+	check_files()
+	bot.add_cog(Converter(bot))
